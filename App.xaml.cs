6a417bd481fa6aa2f2797619794ead2f2b89d0b4
piaomu <driftwood.wahl@gmail.com>
2022-11-06 06:05:02 -0600

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TemCardPlusClientWrapper;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TemCardPlusClientWrapper;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
